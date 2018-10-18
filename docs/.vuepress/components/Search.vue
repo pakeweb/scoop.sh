f97082310cadd470b28705739897986aef98e177
ekoeryanto <ekoeryanto@gmail.com>
2018-10-18 20:53:46 +0700

    chore: stylus formatting

--- docs/.vuepress/components/Search.vue
+++ docs/.vuepress/components/Search.vue
@@ -1,25 +1,25 @@
 <template>
-
   <div class="apps">
     <div class="hero">
       <h1 v-if="query" class="typing">>_ scoop search {{query}}</h1>
       <h1 v-else>Search for Apps</h1>
       <div class="action">
         <div class="search-box app-search-box">
-          <input type="text" v-model="query" @input="search" placeholder="App name" autofocus />
+          <input type="text" v-model="query" @input="search" placeholder="App name" autofocus>
         </div>
         <p class="description" v-if="query && found !== false">
-          <template v-if="found">
-            Found {{found}} apps for query `{{query}}`
-          </template>
-          <template v-else>
-            No app found with query `{{query}}`
-          </template>
+          <template v-if="found">Found {{found}} apps for query `{{query}}`</template>
+          <template v-else>No app found with query `{{query}}`</template>
         </p>
       </div>
     </div>
     <div class="results" style="margin-top: 1.5rem">
-      <modal :is-open="showApp" :click-outside="toggleApp" class="app-modal" overlayClass="app-modal-overlay">
+      <modal
+        :is-open="showApp"
+        :click-outside="toggleApp"
+        class="app-modal"
+        overlayClass="app-modal-overlay"
+      >
         <div v-if="showApp">
           <h3>{{selectedApp.name.replace('.json', '')}} ({{selectedApp.version}})</h3>
           <p>{{selectedApp.description}}</p>
@@ -36,24 +36,20 @@
       <div v-for="app in apps" :key="app.sha" class="result-list" @click="openApp(app)">
         <div class="result-headline">{{app.name.replace('.json', '')}}</div>
         <div class="result-desc">
-          <div>
-            Score: {{app.score}}
-          </div>
-          Bucket:
-          <div>
-            - Name:
-            <Link v-if="!isInternalBucket(app)" :to="app.repository.html_url">{{ app.repository.full_name }}</Link>
-            <span v-else> Internal</span>
+          <div>Score: {{app.score}}</div>Bucket:
+          <div>- Name:
+            <Link
+              v-if="!isInternalBucket(app)"
+              :to="app.repository.html_url"
+            >{{ app.repository.full_name }}</Link>
+            <span v-else>Internal</span>
           </div>
           <div>
             - Description:
             {{app.repository.description}}
           </div>
-          <div>
-            - Author:
-            <Link :to="app.repository.owner.html_url">
-              {{app.repository.owner.login}}
-            </Link>
+          <div>- Author:
+            <Link :to="app.repository.owner.html_url">{{app.repository.owner.login}}</Link>
           </div>
         </div>
       </div>
@@ -129,12 +125,17 @@ export default {
 
   methods: {
     toggleApp() {
-      this.showApp = !this.showApp
+      this.showApp = !this.showApp;
     },
 
     isInternalBucket(o) {
-      const name = typeof o === 'string' ? o : o.repository ? o.repository.full_name : o.full_name
-      return name === 'lukesampson/scoop'
+      const name =
+        typeof o === "string"
+          ? o
+          : o.repository
+            ? o.repository.full_name
+            : o.full_name;
+      return name === "lukesampson/scoop";
     },
 
     async openApp(app) {
@@ -154,8 +155,8 @@ export default {
         ...app,
         ...window.SCOOP_APPS.get(app.sha)
       };
-      this.toggleApp()
-      console.log(this.selectedApp)
+      this.toggleApp();
+      console.log(this.selectedApp);
     },
     search: debounce(function() {
       this.found = false;
@@ -185,65 +186,55 @@ export default {
 
 <style lang="stylus">
 @import '~vue-slim-modal/themes/default.css'
-$borderColor = #eaecef;
 
-.hero {
-  text-align: center;
-}
+$borderColor = #eaecef
 
-.avatar {
-  height: 20px;
-  width: 20px;
-  border-radius: 2px;
-}
+.hero
+  text-align center
 
-.app-search-box {
-  margin-left: 1rem;
-  margin-right: 1rem;
-  width: 95%;
+.avatar
+  height 20px
+  width 20px
+  border-radius 2px
 
-  input {
-    cursor: text;
-    width: 90% !important;
-    border: 1px solid darken($borderColor, 10%);
+.app-search-box
+  margin-left 1rem
+  margin-right 1rem
+  width 95%
 
-    &:focus {
-      cursor: text;
-    }
-  }
-}
+  input
+    cursor text
+    width 90% !important
+    border 1px solid darken($borderColor, 10%)
 
-.typing {
-  overflow: hidden; /* Ensures the content is not revealed until the animation */
-  white-space: nowrap; /* Keeps the content on a single line */
-  animation: typing 3.5s steps(40, end);
-  margin-left: auto;
-  margin-right: auto;
-}
+    &:focus
+      cursor text
 
-@keyframes typing {
-  from {
-    width: 0;
-  }
+.typing
+  overflow hidden /* Ensures the content is not revealed until the animation */
+  white-space nowrap /* Keeps the content on a single line */
+  animation typing 3.5s steps(40, end)
+  margin-left auto
+  margin-right auto
 
-  to {
-    width: 100%;
-  }
-}
+@keyframes typing
+  from
+    width 0
 
-.results {
-  margin: 1.5rem auto;
+  to
+    width 100%
 
-  .result-list {
-    margin: 1rem auto;
-    cursor: pointer;
+.results
+  margin 1.5rem auto
 
-    .result-headline {
-      font-size: 1.5rem;
-      font-weight: bold;
-    }
-  }
-}
+  .result-list
+    margin 1rem auto
+    cursor pointer
+
+    .result-headline
+      font-size 1.5rem
+      font-weight bold
+
 .app-modal
   min-width 70%
   padding 2rem
