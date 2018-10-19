ed75a9fac3c5380d007e6a0e9f71d89b022f7157
ekoeryanto <ekoeryanto@gmail.com>
2018-10-19 08:56:09 +0700

    chore: format stylus on commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  "*.js": ["eslint --fix", "prettier --write", "git add"],
+  "*.js": ["prettier --write", "eslint --fix", "git add"],
   "*.json": ["prettier --write", "git add"],
-  "*.vue": ["eslint --fix", "prettier --write", "git add"],
+  "*.vue": ["prettier --write", "eslint --fix", "git add"],
+  "*.styl": [
+    "stylus-supremacy format --options=.vscode/settings.json -r",
+    "git add"
+  ],
   "*.{png,jpeg,jpg,gif,svg}": ["imagemin-lint-staged", "git add"]
 };
